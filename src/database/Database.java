74c6b4282e1681a7986a1e055bebcd8ab1100c17
Edwin Keville <eddie.kev@gmail.com>
2016-11-18 13:03:25 +0000

    added deletetable()

--- src/database/Database.java
+++ src/database/Database.java
@@ -12,10 +12,12 @@ import java.sql.Statement;
 public class Database {
 
     private static String DB_FILE_NAME = "raftDB.db";
+    private static String TABLE_NAME = "RESULTS";
     Statement stmt = null;
     Connection c = null;
 
     public Database() {
+        deleteTable();
         createTable();
     }
 
@@ -27,7 +29,7 @@ public class Database {
             c = DriverManager.getConnection("jdbc:sqlite:"+ DB_FILE_NAME +"");
 
             stmt = c.createStatement();
-            String sql = "CREATE TABLE RESULTS " +
+            String sql = "CREATE TABLE " +TABLE_NAME +
                     "(ID INTEGER PRIMARY KEY     NOT NULL," +
                     " NAME           TEXT    NOT NULL, " +
                     " VOTES          INT     NOT NULL)";
@@ -49,7 +51,7 @@ public class Database {
             c.setAutoCommit(false);
 
             stmt = c.createStatement();
-            String sql = "INSERT INTO RESULTS (ID,NAME,VOTES) " +
+            String sql = "INSERT INTO "+ TABLE_NAME +" (ID,NAME,VOTES) " +
                         "VALUES (NULL , '" + name + "', 0);";
             stmt.executeUpdate(sql);
 
@@ -74,7 +76,7 @@ public class Database {
             c.setAutoCommit(false);
 
             stmt = c.createStatement();
-            String sql = "UPDATE RESULTS set VOTES = "+(votes+1)+" where NAME=\""+name+"\";";
+            String sql = "UPDATE "+ TABLE_NAME +" set VOTES = "+(votes+1)+" where NAME=\""+name+"\";";
             stmt.executeUpdate(sql);
 
             stmt.close();
@@ -94,7 +96,7 @@ public class Database {
             c.setAutoCommit(false);
 
             stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM RESULTS where NAME=\""+name+"\";");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM "+ TABLE_NAME +" where NAME=\""+name+"\";");
             votesCount = rs.getInt("votes");
             rs.close();
             stmt.close();
@@ -107,22 +109,30 @@ public class Database {
     }
 
 
-    public void printAllRecords(){
+    public String printAllRecords(){
+        String allRecords = "";
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:"+ DB_FILE_NAME +"");
             c.setAutoCommit(false);
 
             stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM RESULTS;" );
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM "+TABLE_NAME+";" );
             while ( rs.next() ) {
                 int id = rs.getInt("id");
                 String  name = rs.getString("name");
                 int votes  = rs.getInt("votes");
-                System.out.println( "ID = " + id );
-                System.out.println( "NAME = " + name );
-                System.out.println( "VOTES = " + votes );
+
+ //               allRecords.concat("ID = "+id + "\n" + "NAME = " + name + "\n" + "VOTES = " + votes + "\n\n");
+                System.out.println( allRecords.concat("ID = " + id ));
+                allRecords.concat("\n");
+                System.out.println( allRecords.concat("NAME = " + name ));
+                allRecords.concat("\n");
+                System.out.println( allRecords.concat("VOTES = " + votes ));
+                allRecords.concat("\n");
+                allRecords.concat("\n");
                 System.out.println();
+
             }
             rs.close();
             stmt.close();
@@ -131,6 +141,28 @@ public class Database {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
+        System.out.println(allRecords);
+        return allRecords;
+    }
+
+    public void deleteTable(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            this.c = DriverManager.getConnection("jdbc:sqlite:" + DB_FILE_NAME + "");
+            this.stmt = this.c.createStatement();
+            String e = "DROP TABLE IF EXISTS "+TABLE_NAME;
+            this.stmt.executeUpdate(e);
+            this.stmt.close();
+            this.c.close();
+        } catch (Exception var2) {
+            System.err.println(var2.getClass().getName() + ": " + var2.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public void deleteDB(){
+        File file = new File(DB_FILE_NAME);
+        file.delete();
     }
 
 
@@ -146,12 +178,7 @@ public class Database {
         db.voteFor("hoops");
         System.out.println("sandy now has: "+db.getVotes("hoops"));
         System.out.println("anne now has: "+db.getVotes("boops"));
-
-
-
-        //deleting db file after testing.
-        File file = new File(DB_FILE_NAME);
-        file.delete();
+        db.printAllRecords();
 
     }
 
